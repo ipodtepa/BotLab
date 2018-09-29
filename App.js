8dab88bb37fc6018e5757f5efff339374e7c0ca6
Igor Podtepa <ipodtepa@gng.com.ua>
2018-09-29 19:19:47 +0000

    to test porposes

--- App.js
+++ App.js
@@ -1,8 +1,16 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 
+var http = require('http');
+
 // Setup Restify Server
 var server = restify.createServer();
+
+function respond(req, res, next) {
+    res.send('hello ' + req.params.name);
+    next();
+  }
+
 server.listen(process.env.port || process.env.PORT || 3982, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
@@ -16,7 +24,20 @@ var connector = new builder.ChatConnector({
 // Listen for messages from users 
 server.post('/api/messages', connector.listen());
 
+// Listen for messages from users 
+//server.get('/test', connector.listen());
+//server.post('/hello/:name', respond);
+server.post('/hello/:name', connector.listen());
+
+
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
     session.send("You said: %s", session.message.text);
 });
+
+
+//create a server object:
+http.createServer(function (req, res) {
+    res.write('Hello World!'); //write a response to the client
+    res.end(); //end the response
+  }).listen(81); //the server object listens on port 8080
